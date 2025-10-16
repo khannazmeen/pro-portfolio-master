c547cda4c2c843d1b6bf2dd6884d6b6c756bd212
Nazmeen <khannazmeen963@gmail.com>
2025-10-16 16:58:46 +0530

    make profile image svg

--- src/assets/default.exports.ts
+++ src/assets/default.exports.ts
@@ -1,5 +1,5 @@
 export { default as Favicon } from "./images/favicon.png";
-export { default as Illustration } from "./images/profile.png";
+export { default as Illustration } from "./images/profile.svg";
 // export { default as Profile } from "./images/profile.png";
 export { default as ProjectImage } from "./images/project.png";
 export { default as WebDeveloper } from "./images/web-developer.png";
